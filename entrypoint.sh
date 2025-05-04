2920c3f22bd394aaf24614059891740681668e67
evertonccarvalho <evertonsnake@gmail.com>
2025-05-04 12:12:25 -0300

    Remove obsolete NPC scripts and add new Curandeira script for VIP services. Updated script configuration to reflect changes in NPC availability and functionality.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Run the original MySQL entrypoint
+# Inicia o MySQL em segundo plano
 /usr/local/bin/docker-entrypoint.sh mysqld &
 
-# Wait for MySQL to start
-while ! mysqladmin ping -hlocalhost --silent; do
+# Aguarda o MySQL estar pronto para conexões
+until mysqladmin ping -hlocalhost --silent; do
     sleep 1
 done
 
-# Alterar a autenticação do usuário 'ragnarok' para 'mysql_native_password'
-echo "Alterando autenticação do usuário ragnarok para mysql_native_password..."
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'ragnarok'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';"
+# Cria o usuário 'ragnarok' se ainda não existir e altera a autenticação para 'mysql_native_password'
+echo "Configurando o usuário 'ragnarok'..."
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
+    CREATE USER IF NOT EXISTS 'ragnarok'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    ALTER USER 'ragnarok'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ragnarok.* TO 'ragnarok'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
 
-# Verificar as permissões do usuário 'ragnarok'
-echo "Verificando permissões do usuário ragnarok..."
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW GRANTS FOR 'ragnarok'@'%';"
-
-# Conceder todas as permissões ao usuário 'ragnarok'
-echo "Concedendo todas as permissões ao usuário ragnarok..."
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ragnarok.* TO 'ragnarok'@'%';"
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-
-# Executa APENAS os scripts que você quer
+# Executa os scripts SQL no banco de dados 'ragnarok'
+echo "Executando scripts SQL..."
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" ragnarok < /docker-entrypoint-initdb.d/main.sql
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" ragnarok < /docker-entrypoint-initdb.d/web.sql
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" ragnarok < /docker-entrypoint-initdb.d/logs.sql
-# Keep container running
+
+# Mantém o container ativo
 wait
